f5b4912ae8e499d41ecd80524082033786ae4f5a
sahilthorat3132 <128784426+sahilthorat3132@users.noreply.github.com>
2025-07-08 22:51:11 +0530

    Update store.routes.js

--- backend/routes/store.routes.js
+++ backend/routes/store.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/store.controller');
-const auth = require('../middleware/auth.middleware'); // this is okay if above is correct
+const auth = require('../middleware/auth.middleware'); 
 
 router.get('/', auth(['user']), storeController.getAllStores);
 router.get('/my', auth(['owner']), storeController.getStoreRatingsForOwner);
